db53b5e89b7e293338169ee66fa61c1d5706be17
Raphaël Vinot <raphael@vinot.info>
2020-03-10 15:15:41 +0100

    fix: Properly define script

---
+++ pytaxonomies/script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+from pytaxonomies import Taxonomies
+
+
+def main():
+    argParser = argparse.ArgumentParser(description='Use MISP taxonomies')
+    argParser.add_argument('-a', '--all', action='store_true', help='Print all taxonomies as machine tags')
+    argParser.add_argument('-l', '--local', default=None, help='Use local manifest file.')
+    args = argParser.parse_args()
+    if args.local:
+        t = Taxonomies(manifest_path=args.local)
+    else:
+        t = Taxonomies()
+    if args.all:
+        print(t)
